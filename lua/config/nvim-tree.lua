01cffabb544f352603bd92fcc2b546e24ac08054
marko <marko03kostic@protonmail.com>
2025-03-20 20:56:46 +0100

    changed up the file structure, added lazy and nvim-tree

---
+++ lua/config/nvim-tree.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
