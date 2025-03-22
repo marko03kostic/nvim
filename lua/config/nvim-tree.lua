aaf8eaa124c7232e949c8ae24cf9c813af244ab2
Marko Kostic <marko03kostic@protonmail.com>
2025-03-22 15:35:01 +0100

    added findfile keymap

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', ':NvimTreeClose<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>c', ':NvimTreeCollapse<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
