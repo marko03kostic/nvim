045e22959452215304933a277b5a5300fa782bc5
marko <marko03kostic@protonmail.com>
2025-03-27 23:02:52 +0100

    added material theme

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.g.maplocalleader = "\\"
 
 require("config.lazy")
 require("config.lualine")
+require("config.material")
 require("config.nvim-tree")
 require("config.telescope-fzf-finder")
 require("config.telescope")
