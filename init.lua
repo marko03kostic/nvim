53741c86c227e3164e2b89f43bcab5ae2aebb265
Marko Kostic <marko03kostic@protonmail.com>
2025-03-27 18:07:52 +0100

    added telescope

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("config.lazy")
-require("plugins.nvim-tree")
 require("config.nvim-tree")
+require("config.nvim-telescope")
--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', ':NvimTreeClose<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>c', ':NvimTreeCollapse<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
