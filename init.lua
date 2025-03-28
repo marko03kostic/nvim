084b99528504e348102d9771423218c2569fa087
Marko Kostic <marko03kostic@protonmail.com>
2025-03-28 09:30:30 +0100

    added copy paste functionalities

--- init.lua
+++ init.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
 vim.api.nvim_set_keymap('n', '<leader>`', ':e ~/.config/nvim/<CR>', { noremap = true, silent = true , desc = "Nvim config"})
+
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"+yy', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true, silent = true })
 
 require("config.lazy")
 require("config.lualine")
